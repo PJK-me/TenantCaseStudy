a285150a8d9e1b87f4149ee73c8fcc61999196f1
PJK-me <geekarus.it@gmail.com>
2025-02-11 16:35:34 +0100

    Added Tenant and Domain models with initial migrations

--- tenants/models.py
+++ tenants/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tenant(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Domain(models.Model):
+    tenant = models.OneToOneField(Tenant, on_delete=models.CASCADE, related_name='domains')
+    domain_url = models.URLField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.domain_url
