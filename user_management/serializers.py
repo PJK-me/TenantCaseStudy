1c3898a9621ca97214fea4729302ad22f322dda3
PJK-me <geekarus.it@gmail.com>
2025-02-19 03:59:19 +0100

    Updated serializer for BaseUser to increase data isolation between tenants and users limited to specific scopes

--- user_management/serializers.py
+++ user_management/serializers.py
@@ -7,8 +7,9 @@ from .utils import Role, TENANT_DEPENDANT_ROLES
 class BaseUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'role', 'tenant_scope', 'organization_scope',
-                  'department_scope', 'customer_scope', 'is_active', 'is_superuser']
+        fields = ['id', 'local_id', 'username', 'role', 'tenant_scope', 'organization_scope',
+                  'department_scope', 'customer_scope', 'is_deleted', 'is_superuser']
+        read_only_fields = ['id', 'local_id', 'role', 'is_deleted']
 
     def validate_role(self, value):
         if value not in Role.choices:
@@ -17,7 +18,7 @@ class BaseUserSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         if data.get('role') in TENANT_DEPENDANT_ROLES and not data.get('tenant_scope'):
-            raise ValidationError({"tenant_scope": "This role requires tenant scope."})
+            raise ValidationError("This role requires tenant scope.")
         return data
 
     def create(self, validated_data):
@@ -28,3 +29,34 @@ class BaseUserSerializer(serializers.ModelSerializer):
         if 'tenant_scope' in validated_data and not instance.is_superuser:
             raise ValidationError("You cannot change tenant scope.")
         return super().update(instance, validated_data)
+
+    def to_representation(self, instance):
+        user = self.context['request'].user
+        data = super().to_representation(instance)
+
+        if not user.is_admin():
+            data["id"] = data["local_id"]
+            data.pop("local_id", None)
+            data.pop("is_deleted", None)
+
+            if "organization_scope" in data:
+                if getattr(instance, "organization_scope", None) is not None:
+                    data["organization_scope"] = instance.organization_scope.local_id
+                else:
+                    data.pop("organization_scope", None)
+
+            if "department_scope" in data:
+                if getattr(instance, "department_scope", None) is not None:
+                    data["department_scope"] = instance.department_scope.local_id
+                else:
+                    data.pop("department_scope", None)
+
+            if "customer_scope" in data:
+                if getattr(instance, "customer_scope", None) is not None:
+                    data["customer_scope"] = instance.organization_scope.local_id
+                else:
+                    data.pop("customer_scope", None)
+
+        return data
+
+
