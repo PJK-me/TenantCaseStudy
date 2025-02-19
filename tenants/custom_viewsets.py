7be61c9c0297860365188e694e0f8b7c033b3b51
PJK-me <geekarus.it@gmail.com>
2025-02-19 04:02:20 +0100

    Updated viewsets of main hierarchical models, while keeping them thin

--- tenants/custom_viewsets.py
+++ tenants/custom_viewsets.py
@@ -1,19 +1,72 @@
+from rest_framework import status
+from rest_framework.exceptions import PermissionDenied, NotFound
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-
 from tenants.mixins import TenantMixin
+from user_management.models import BaseUser
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 class TenantScopedModelViewSet(TenantMixin, ModelViewSet):
 
     def get_queryset(self):
-        return super().get_queryset()
+        user = self.request.user
 
-    def perform_create(self, serializer):
-        tenant = self.get_tenant()
-        serializer.save(tenant=tenant)
+        if not user.is_authenticated or not isinstance(user, BaseUser):
+            return super().get_queryset().none()
+
+        model = self.queryset.model
+        return user.get_limited_queryset(model)
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
         tenant = self.get_tenant()
         context["tenant"] = tenant
         return context
+
+    def soft_delete_cascade(self, instance):
+        soft_deleted_objects = []
+        for relation in instance._meta.related_objects:
+            # I'm ignoring ManyToMany relations, because they dont exist in current implementation
+            if not relation.auto_created or not relation.related_model:
+                continue
+
+            accessor = relation.get_accessor_name()
+            related_manager = getattr(instance, accessor, None)
+            if related_manager:
+                for obj in related_manager.all():
+                    if hasattr(obj, "is_deleted"):
+                        if not obj.is_deleted:
+                            soft_deleted_objects.append(str(obj))
+                            obj.soft_delete()
+        return soft_deleted_objects
+
+    def soft_delete(self, instance):
+        if instance.is_deleted:
+            return Response({"soft_deletion": f"{instance} is already soft deleted"},status=status.HTTP_410_GONE)
+
+        instance.soft_delete()
+
+        soft_deleted_objects = self.soft_delete_cascade(instance)
+
+        return Response({"soft_deletion": f"Performed soft delete on {instance}", "soft_deletion_cascade": soft_deleted_objects}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, *args, **kwargs):
+        user = self.request.user
+        if not user.is_authenticated or not isinstance(user, BaseUser):
+            raise PermissionDenied("Authentication is required for access")
+
+        if user.is_admin() or user.is_superuser:
+            return super().destroy(request, *args, **kwargs)
+        else:
+            instance = self.get_object()
+            return self.soft_delete(instance)
+
+    def get_object(self):
+        user = self.request.user
+        model = self.queryset.model
+        obj_id = self.kwargs.get("pk")
+        return user.get_limited_object(model, obj_id)
+
+
--- tenants/views.py
+++ tenants/views.py
@@ -1,128 +1,56 @@
 import logging
+
+from django.contrib.auth import get_user_model
 from rest_framework import status
+from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, PermissionDenied
+from rest_framework.viewsets import ModelViewSet
 
 from .custom_viewsets import TenantScopedModelViewSet
 from .serializers import TenantSerializer, OrganizationSerializer, DepartmentSerializer, CustomerSerializer
 from .decorators import tenant_scope_required
-from .models import Organization, Department, Customer
+from .models import Organization, Department, Customer, Tenant
 
 
-class TenantCreateView(APIView):
+class TenantViewSet(ModelViewSet):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+    permission_classes = [DjangoModelPermissions]
 
-    def post(self, request, *args, **kwargs):
-        serializer = TenantSerializer(data=request.data)
-        if serializer.is_valid():
-            tenant = serializer.save()
-            return Response(TenantSerializer(tenant).data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        user = self.request.user
+
+        if not user.is_authenticated or not isinstance(user, get_user_model()):
+            return super().get_queryset().none()
+
+        model = self.queryset.model
+        return user.get_limited_queryset(model)
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        if not (user.is_admin() or user.is_superuser):
+            return Response({"error": "Permission denied."}, status=403)
+        return super().create(request, *args, **kwargs)
 
 
 @tenant_scope_required
 class OrganizationViewSet(TenantScopedModelViewSet):
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
-
-    def get_queryset(self):
-        tenant = self.get_tenant()
-        return Organization.objects.filter(tenant=tenant)
-
-    def retrieve(self, request, *args, **kwargs):
-        tenant = self.get_tenant()
-
-        try:
-            organization = Organization.objects.get(id=kwargs["pk"], tenant=tenant)
-        except Organization.DoesNotExist:
-            return Response({"error": "Organization not found for the specified organization."}, status=404)
-
-        serializer = self.get_serializer(organization)
-        return Response(serializer.data)
-
-    def perform_create(self, serializer):
-        tenant = self.get_tenant()
-        serializer.save(tenant=tenant)
+    permission_classes = [DjangoModelPermissions]
 
 
 @tenant_scope_required
 class DepartmentViewSet(TenantScopedModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
-
-    def get_queryset(self):
-        tenant = self.get_tenant()
-        return Department.objects.filter(organization__tenant=tenant)
-
-    def retrieve(self, request, *args, **kwargs):
-        tenant = self.get_tenant()
-        organization_id = self.kwargs.get("organization_id")
-
-        try:
-            organization = Organization.objects.get(id=organization_id, tenant=tenant)
-        except Organization.DoesNotExist:
-            raise ValidationError({"organization": "Organization does not exist for this tenant."})
-
-        try:
-            department = Department.objects.get(id=kwargs["pk"], organization=organization)
-        except Department.DoesNotExist:
-            return Response({"error": "Department not found for the specified organization."}, status=404)
-
-        serializer = self.get_serializer(department)
-        return Response(serializer.data)
-
-    def perform_create(self, serializer):
-        tenant = self.get_tenant()
-        target_organization = serializer.validated_data.get("organization")
-
-        if not target_organization:
-            raise ValidationError({"organization": "This field is required."})
-
-        try:
-            organization = Organization.objects.get(id=target_organization.id, tenant=tenant)
-        except Organization.DoesNotExist:
-            raise ValidationError({"organization": "Invalid organization for the current tenant."})
-
-        serializer.save(organization=organization)
-
+    permission_classes = [DjangoModelPermissions]
 
 
 @tenant_scope_required
 class CustomerViewSet(TenantScopedModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-
-    def get_queryset(self):
-        tenant = self.get_tenant()
-        return Customer.objects.filter(department__organization__tenant=tenant)
-
-    def retrieve(self, request, *args, **kwargs):
-        tenant = self.get_tenant()
-        department_id = self.kwargs.get("department_id")
-
-        try:
-            department = Department.objects.get(id=department_id, organization__tenant=tenant)
-        except Department.DoesNotExist:
-            raise ValidationError({"department": "Department does not exist for this tenant."})
-
-        try:
-            customer = Customer.objects.get(id=kwargs["pk"], department=department)
-        except Customer.DoesNotExist:
-            return Response({"error": "Customer not found for the specified department."}, status=404)
-
-        serializer = self.get_serializer(customer)
-        return Response(serializer.data)
-
-    def perform_create(self, serializer):
-        tenant = self.get_tenant()
-        target_department = serializer.validated_data.get("department")
-
-        if not target_department:
-            raise ValidationError({"department": "This field is required."})
-
-        try:
-            department = Department.objects.get(id=target_department.id, organization__tenant=tenant)
-        except Department.DoesNotExist:
-            raise ValidationError({"department": "Invalid department for the current tenant."})
-
-        serializer.save(department=department)
+    permission_classes = [DjangoModelPermissions]
