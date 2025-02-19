47670be37eadd82191f2c43e9c526a7200bf0d88
PJK-me <geekarus.it@gmail.com>
2025-02-19 03:56:08 +0100

    Added 'is_deleted', 'deleted_at' and 'local_id' fields for BaseUser to allow soft deletion and further data isolation - by using get_limited_* methods, querysets will be limited to respective scopes of users

--- user_management/models.py
+++ user_management/models.py
@@ -1,66 +1,209 @@
 from django.apps import apps
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-from rest_framework.exceptions import ValidationError
+from django.db.models import Window, F
+from django.db.models.functions import RowNumber
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser, Permission, BaseUserManager
+from django.db import models, connection
+from rest_framework.exceptions import ValidationError, NotFound
 
-from user_management.utils import (Role, ROLE_PERMISSIONS, TENANT_DEPENDANT_ROLES, ORGANIZATION_DEPENDANT_ROLES,
-                                   DEPARTMENT_DEPENDANT_ROLES, CUSTOMER_DEPENDANT_ROLES, ROLE_HIERARCHY)
+from user_management.utils import (Role, TENANT_DEPENDANT_ROLES, ORGANIZATION_DEPENDANT_ROLES,
+                                   DEPARTMENT_DEPENDANT_ROLES, CUSTOMER_DEPENDANT_ROLES, ROLE_HIERARCHY,
+                                   RolePermissionsManager)
 
+class TenantBaseUserManager(BaseUserManager):
+    def create_user(self, username, password, is_admin=False, tenant_scope=None, organization_scope=None, department_scope=None, customer_scope=None,  **extra_fields):
+
+        if is_admin:
+            role = Role.ROLE_TENANT_ADMIN
+        else:
+            if customer_scope is not None:
+                role = Role.ROLE_CUSTOMER_USER
+            elif department_scope is not None:
+                role = Role.ROLE_DEPT_USER
+            elif organization_scope is not None:
+                role = Role.ROLE_ORG_USER
+            elif tenant_scope is not None:
+                role = Role.ROLE_TENANT_USER
+            else:
+                role = Role.ROLE_UNNASIGNED
+
+        user = self.model(
+            username=username,
+            role=role,
+            tenant_scope=tenant_scope,
+            organization_scope=organization_scope,
+            department_scope=department_scope,
+            customer_scope=customer_scope,
+            **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username, password, True, **extra_fields)
 
 class BaseUser(AbstractUser):
+    local_id = models.PositiveIntegerField(null=True, blank=True)
     role = models.PositiveSmallIntegerField(choices=Role.choices, default=Role.ROLE_UNNASIGNED)
-    tenant_scope = models.ForeignKey("tenants.Tenant", on_delete=models.PROTECT, null=True, blank=True, db_index=True)
-    organization_scope = models.ForeignKey("tenants.Organization", on_delete=models.PROTECT, null=True, blank=True, db_index=True)
-    department_scope = models.ForeignKey("tenants.Department", on_delete=models.PROTECT, null=True, blank=True, db_index=True)
-    customer_scope = models.ForeignKey("tenants.Customer", on_delete=models.PROTECT, null=True, blank=True, db_index=True)
+    tenant_scope = models.ForeignKey("tenants.Tenant", on_delete=models.CASCADE, null=True, blank=True, db_index=True)
+    organization_scope = models.ForeignKey("tenants.Organization", on_delete=models.CASCADE, null=True, blank=True, db_index=True)
+    department_scope = models.ForeignKey("tenants.Department", on_delete=models.CASCADE, null=True, blank=True, db_index=True)
+    customer_scope = models.ForeignKey("tenants.Customer", on_delete=models.CASCADE, null=True, blank=True, db_index=True)
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = TenantBaseUserManager()
+
+    def __str__(self):
+        return self.username
 
     def is_admin(self):
-        return self.role == Role.ROLE_TENANT_ADMIN
+        return self.role == Role.ROLE_TENANT_ADMIN or self.is_superuser or self.is_staff
 
     def can_assign_role(self, target_user, role):
-        if role not in ROLE_HIERARCHY.get(self.role, set()):
+        role_manager = RolePermissionsManager()
+        if role not in role_manager.get_role_hierarchy(self.role):
             return False
 
-        if role in TENANT_DEPENDANT_ROLES and target_user.tenant_scope != self.tenant_scope:
+        if role_manager.is_role_tenant_dependant(self.role) and target_user.tenant_scope != self.tenant_scope:
             return False
-        if role in ORGANIZATION_DEPENDANT_ROLES and target_user.organization_scope != self.organization_scope:
+        if role_manager.is_role_organization_dependant(self.role) and target_user.organization_scope != self.organization_scope:
             return False
-        if role in DEPARTMENT_DEPENDANT_ROLES and target_user.department_scope != self.department_scope:
+        if role_manager.is_role_department_dependant(self.role) and target_user.department_scope != self.department_scope:
             return False
-        if role in CUSTOMER_DEPENDANT_ROLES and target_user.customer_scope != self.customer_scope:
+        if self.role is Role.ROLE_CUSTOMER_USER and target_user.customer_scope != self.customer_scope:
             return False
 
         return True
 
+    def get_limited_object(self, model, obj_id):
+        if self.is_admin():
+            return self.get_limited_object_by_id(model, obj_id)
+        return self.get_limited_object_by_local_id(model, obj_id)
+
+    def get_limited_object_by_id(self, model, obj_id):
+        try:
+            return self.get_limited_queryset(model).get(id=obj_id)
+        except model.DoesNotExist:
+            raise NotFound(f"{model._meta.model_name} instance with id == '{obj_id}' not found in the scope of this user")
+
+    def get_limited_object_by_local_id(self, model, obj_id):
+        try:
+            return self.get_limited_queryset(model).get(local_id=obj_id)
+        except model.DoesNotExist:
+            raise NotFound(f"{model._meta.model_name} instance with local_id == '{obj_id}' not found in the scope of this user")
+
     def get_limited_queryset(self, model):
-        role_filters = {
-            Role.ROLE_TENANT_ADMIN: {
+        """
+        Returns a queryset of objects limited to a specific model.
+        Using model_role_filters, we get filters that specific role would use to get queryset
+        By default Role.ROLE_TENANT_ADMIN, superusers and staff are not limited in this way
+        """
+        model_name = model._meta.model_name
 
+        if self.is_admin() or self.is_superuser:
+            return model.objects.all()
+
+        # Depending on model, we change filters that each user role will use to get the queryset
+        model_role_filters = {
+            "tenant": {
+                Role.ROLE_TENANT_USER: {
+                    "id": self.tenant_scope.id if self.tenant_scope else None,
+                    "is_deleted": False
+                },
             },
-            Role.ROLE_TENANT_USER: {
-                "tenant": self.tenant_scope
+            "organization": {
+                Role.ROLE_TENANT_USER: {
+                    "tenant": self.tenant_scope.id if self.tenant_scope else None,
+                    "is_deleted": False
+                },
+                Role.ROLE_ORG_USER: {
+                    "id": self.organization_scope.id if self.organization_scope else None,
+                    "tenant": self.tenant_scope,
+                    "is_deleted": False
+                },
             },
-            Role.ROLE_ORG_USER: {
-                "organization": self.organization_scope,
-                "organization__tenant": self.tenant_scope
+            "department": {
+                Role.ROLE_TENANT_USER: {
+                    "organization__tenant": self.tenant_scope.id if self.tenant_scope else None,
+                    "is_deleted": False
+                },
+                Role.ROLE_ORG_USER: {
+                    "organization": self.organization_scope.id if self.organization_scope else None,
+                    "organization__tenant": self.tenant_scope,
+                    "is_deleted": False
+                },
+                Role.ROLE_DEPT_USER: {
+                    "id": self.department_scope.id if self.department_scope else None,
+                    "organization": self.organization_scope,
+                    "organization__tenant": self.tenant_scope,
+                    "is_deleted": False
+                },
             },
-            Role.ROLE_DEPT_USER: {
-                "department": self.department_scope,
-                "department__organization": self.organization_scope,
-                "department__organization__tenant": self.tenant_scope,
-                "customer__department": self.department_scope
+            "customer": {
+                Role.ROLE_TENANT_USER: {
+                    "department__organization__tenant": self.tenant_scope.id if self.tenant_scope else None,
+                    "is_deleted": False
+                },
+                Role.ROLE_ORG_USER: {
+                    "department__organization": self.organization_scope.id if self.organization_scope else None,
+                    "department__organization__tenant": self.tenant_scope,
+                    "is_deleted": False
+                },
+                Role.ROLE_DEPT_USER: {
+                    "department": self.department_scope.id if self.department_scope else None,
+                    "department__organization": self.organization_scope,
+                    "department__organization__tenant": self.tenant_scope,
+                    "is_deleted": False
+                },
+                Role.ROLE_CUSTOMER_USER: {
+                    "id": self.customer_scope.id if self.customer_scope else None,
+                    "department": self.department_scope,
+                    "department__organization": self.organization_scope,
+                    "department__organization__tenant": self.tenant_scope,
+                    "is_deleted": False
+                },
             },
-            Role.ROLE_CUSTOMER_USER: {
-                "id": self.customer_scope.id if self.customer_scope else None
+            "baseuser": {
+                Role.ROLE_TENANT_USER: {
+                    "tenant_scope": self.tenant_scope,
+                    "is_deleted": False
+                },
+                Role.ROLE_ORG_USER: {
+                    "organization_scope": self.organization_scope,
+                    "tenant_scope": self.tenant_scope,
+                    "is_deleted": False
+                },
+                Role.ROLE_DEPT_USER: {
+                    "department_scope": self.department_scope,
+                    "organization_scope": self.organization_scope,
+                    "tenant_scope": self.tenant_scope,
+                    "is_deleted": False
+                },
+                Role.ROLE_CUSTOMER_USER: {
+                    "customer_scope": self.customer_scope,
+                    "department_scope": self.department_scope,
+                    "organization_scope": self.organization_scope,
+                    "tenant_scope": self.tenant_scope,
+                    "is_deleted": False
+                },
             },
 
-            # Role.ROLE_UNNASIGNED is not provided, so we can return model.objects.none()
         }
 
-        filters = role_filters.get(self.role, {})
+        filters = model_role_filters.get(model_name, {}).get(self.role, {})
 
-        if self.role == Role.ROLE_TENANT_ADMIN:
-            return model.objects.all()
+        # if filters are empty - then it means that Role does not allow to access any instances of that model
         if not filters or (self.role == Role.ROLE_CUSTOMER_USER and not self.customer_scope):
             return model.objects.none()
 
@@ -73,12 +216,15 @@ class BaseUser(AbstractUser):
         return queryset
 
     def has_perm(self, perm, obj=None):
-        if perm not in ROLE_PERMISSIONS.get(self.role, set()):
+        role_manager = RolePermissionsManager()
+
+        if perm not in role_manager.get_role_permissions(self.role):
             return False
 
         if self.role == Role.ROLE_UNNASIGNED:
             return False
 
+        # if we are not checking obj permissions, then we can return True
         if not obj:
             return True
 
@@ -144,14 +290,86 @@ class BaseUser(AbstractUser):
         if self.role in CUSTOMER_DEPENDANT_ROLES and not self.customer_scope:
             raise ValidationError({"customer_scope": "This role requires customer."})
 
-    def save(self, *args, **kwargs):
-        if self.pk and not self.is_superuser:
-            scope_fields = ["tenant_scope", "organization_scope", "department_scope", "customer_scope"]
+    def _assign_permission(self, permission):
+        app_label, perm_codename = permission.split(".")
+        permission = Permission.objects.filter(codename=perm_codename, content_type__app_label=app_label).first()
+        if permission and permission not in self.user_permissions.all():
+            self.user_permissions.add(permission)
 
-            old_instance = BaseUser.objects.filter(pk=self.pk).values(*scope_fields).first()
+    def _assign_role_permissions(self, role_manager):
+        if self.role is not None:
+            for permission in role_manager.get_role_permissions(self.role):
+                self._assign_permission(permission)
 
-            for field in scope_fields:
-                if old_instance.get(field) != getattr(self, field):
-                    raise ValidationError({field: f"You cannot change your {field.replace('_', ' ')}."})
+    def _reset_role_permissions(self, role_manager):
+        self.user_permissions.clear()
+        self._assign_role_permissions(role_manager)
+
+    def soft_delete(self):
+        if self.is_deleted:
+            return
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save(skip_scope_validation=True, update_fields=["is_deleted", "deleted_at"])
+
+    def restore(self):
+        if self.is_deleted:
+            return
+        self.is_deleted = False
+        self.deleted_at = None
+        self.save(skip_scope_validation=True, update_fields=["is_deleted", "deleted_at"])
+
+    def get_lowest_scope(self):
+        if self.customer_scope:
+            return "customer"
+        if self.department_scope:
+            return "department"
+        if self.organization_scope:
+            return "organization"
+        if self.tenant_scope:
+            return "tenant"
+        return None
+
+    def save(self, *args, skip_scope_validation=False, **kwargs):
+        initial_setup = self.pk is None
+        role_manager = RolePermissionsManager()
+
+        if self.local_id is None and not self.is_admin():
+            tenant = self.tenant_scope
+            if tenant:
+                schema_name = tenant.get_schema_name()
+                model_name = self.get_lowest_scope()
+
+                sequence_name = f"{schema_name}.{model_name}_user_local_id"
+
+                with connection.cursor() as cursor:
+                    cursor.execute(f"SELECT nextval('{sequence_name}')")
+                    self.local_id = cursor.fetchone()[0]
+
+        if self.pk and not skip_scope_validation:
+
+            if not self.is_admin() or not self.is_superuser:
+                scope_fields = ["tenant_scope", "organization_scope", "department_scope", "customer_scope", "role"]
+
+                old_instance = BaseUser.objects.get(pk=self.pk)
+
+                for field in scope_fields:
+                    if getattr(old_instance, field) != getattr(self, field):
+                        raise ValidationError(
+                            {
+                                field: f"You cannot change your {field.replace('_', ' ')}.",
+                             }
+                        )
+
+                if old_instance.role != self.role:
+                    self._reset_role_permissions(role_manager)
 
         super().save(*args, **kwargs)
+
+        if initial_setup:
+            # assigning roles, happens after creating id (necessary for many to many fields)
+            self._assign_role_permissions(role_manager)
+
+
+
+
--- user_management/utils.py
+++ user_management/utils.py
@@ -9,40 +9,101 @@ class Role(models.IntegerChoices):
     ROLE_DEPT_USER = 4, "ROLE_DEPT_USER"
     ROLE_CUSTOMER_USER = 5, "ROLE_CUSTOMER_USER"
 
-ROLE_PERMISSIONS = {
-    Role.ROLE_TENANT_ADMIN: {"view_tenant", "change_tenant", "view_organization", "change_organization", "view_department", "change_department", "view_customer", "change_customer"},
-    Role.ROLE_TENANT_USER: {"view_tenant", "view_organization", "change_organization", "view_department", "change_department", "view_customer", "change_customer"},
-    Role.ROLE_ORG_USER: {"view_tenant", "view_organization", "view_department", "change_department", "view_customer", "change_customer"},
-    Role.ROLE_DEPT_USER: {"view_department", "view_customer", "change_customer"},
-    Role.ROLE_CUSTOMER_USER: {"view_customer"},
-}
+
+class RolePermissionsManager:
+    BASE_ROLE_HIERARCHY = [
+        Role.ROLE_CUSTOMER_USER,
+        Role.ROLE_DEPT_USER,
+        Role.ROLE_ORG_USER,
+        Role.ROLE_TENANT_USER,
+        Role.ROLE_TENANT_ADMIN,
+    ]
+
+    BASE_ROLE_PERMISSIONS = {
+        Role.ROLE_CUSTOMER_USER: {"tenants.view_customer"},
+        Role.ROLE_DEPT_USER: {"tenants.view_department", "tenants.change_customer", "tenants.add_customer", "tenants.delete_customer"},
+        Role.ROLE_ORG_USER: { "tenants.view_organization", "tenants.change_department", "tenants.add_department", "tenants.delete_department"},
+        Role.ROLE_TENANT_USER: {"tenants.view_tenant", "tenants.change_organization", "tenants.add_organization", "tenants.delete_organization"},
+        Role.ROLE_TENANT_ADMIN: {"tenants.add_tenant", "tenants.change_tenant", "tenants.delete_tenant"},
+    }
+
+    def __init__(self):
+        self.ROLE_PERMISSIONS = {}
+        self.ROLE_HIERARCHY = {}
+
+        accumulated_permissions = set()
+        accumulated_roles = set()
+        for role in self.BASE_ROLE_HIERARCHY:
+            accumulated_permissions |= self.BASE_ROLE_PERMISSIONS.get(role, set())
+            self.ROLE_PERMISSIONS[role] = frozenset(accumulated_permissions)
+
+            self.ROLE_HIERARCHY[role] = accumulated_roles
+            accumulated_roles.add(role)
+
+    def get_role_permissions(self, role):
+        return self.ROLE_PERMISSIONS.get(role, set())
+
+    def get_role_hierarchy(self, role):
+        return self.ROLE_HIERARCHY.get(role, set())
+
+    def is_role_tenant_dependant(self, role):
+        return role in {
+            Role.ROLE_CUSTOMER_USER,
+            Role.ROLE_DEPT_USER,
+            Role.ROLE_ORG_USER,
+            Role.ROLE_TENANT_USER,
+        }
+
+    def is_role_organization_dependant(self, role):
+        return role in {
+            Role.ROLE_CUSTOMER_USER,
+            Role.ROLE_DEPT_USER,
+            Role.ROLE_ORG_USER,
+        }
+
+    def is_role_department_dependant(self, role):
+        return role in {
+            Role.ROLE_CUSTOMER_USER,
+            Role.ROLE_DEPT_USER,
+        }
+
+    def is_role_customer_dependant(self, role):
+        return role in {
+            Role.ROLE_CUSTOMER_USER,
+        }
+
+
 
 ROLE_HIERARCHY = {
-        Role.ROLE_TENANT_ADMIN: {Role.ROLE_TENANT_USER, Role.ROLE_ORG_USER, Role.ROLE_DEPT_USER, Role.ROLE_CUSTOMER_USER},
-        Role.ROLE_TENANT_USER: {Role.ROLE_ORG_USER, Role.ROLE_DEPT_USER, Role.ROLE_CUSTOMER_USER},
-        Role.ROLE_ORG_USER: {Role.ROLE_DEPT_USER, Role.ROLE_CUSTOMER_USER},
-        Role.ROLE_DEPT_USER: {Role.ROLE_CUSTOMER_USER},
-        Role.ROLE_CUSTOMER_USER: set(),
+    Role.ROLE_CUSTOMER_USER: set(),
+    Role.ROLE_DEPT_USER: {Role.ROLE_CUSTOMER_USER},
+    Role.ROLE_ORG_USER: {Role.ROLE_DEPT_USER, Role.ROLE_CUSTOMER_USER},
+    Role.ROLE_TENANT_USER: {Role.ROLE_ORG_USER, Role.ROLE_DEPT_USER, Role.ROLE_CUSTOMER_USER},
+    Role.ROLE_TENANT_ADMIN: {Role.ROLE_TENANT_USER, Role.ROLE_ORG_USER, Role.ROLE_DEPT_USER, Role.ROLE_CUSTOMER_USER},
+}
+
+TENANT_ADMIN_DEPENDANT_ROLES = {
+
 }
 
 TENANT_DEPENDANT_ROLES = {
-    Role.ROLE_TENANT_USER,
-    Role.ROLE_ORG_USER,
+    Role.ROLE_CUSTOMER_USER,
     Role.ROLE_DEPT_USER,
-    Role.ROLE_CUSTOMER_USER
+    Role.ROLE_ORG_USER,
+    Role.ROLE_TENANT_USER,
 }
 
 ORGANIZATION_DEPENDANT_ROLES = {
-    Role.ROLE_ORG_USER,
+    Role.ROLE_CUSTOMER_USER,
     Role.ROLE_DEPT_USER,
-    Role.ROLE_CUSTOMER_USER
+    Role.ROLE_ORG_USER,
 }
 
 DEPARTMENT_DEPENDANT_ROLES = {
+    Role.ROLE_CUSTOMER_USER,
     Role.ROLE_DEPT_USER,
-    Role.ROLE_CUSTOMER_USER
 }
 
 CUSTOMER_DEPENDANT_ROLES = {
-    Role.ROLE_CUSTOMER_USER
+    Role.ROLE_CUSTOMER_USER,
 }
